9206a836d13f197a77dd9e3e21d31f5b4aa61a22
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-08-25 10:43:36 -0300

    Criação de header no auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import Head from "next/head"
+import HeaderNoAuth from "../src/components/homeNoAut/headerNoAuth";
 import  styles  from "../styles/HomeNoAuth.module.scss"
+
 
 
 const HomeNoAuth = () =>{
   return(
     <>
-    <Head><title>Home</title></Head>
-    <main></main>
+    <Head>
+    <title>OneBitFlix</title>
+    <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>
+    <meta property="og:title" content="Onebitflix" key="title"/>
+    <meta name="description" content="Onebitflix" key="title"/>
+    </Head>
+    <main>
+      <HeaderNoAuth/>
+    </main>
     </>
   );
 };
 
-export default HomeNoAuth
+export default HomeNoAuth;
