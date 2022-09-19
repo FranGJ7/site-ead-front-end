ef4e48bd7b9afba7880d53a0e18220c689f72bee
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-09-19 19:12:48 -0300

    Conexão com back end parte de registro

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,8 +3,53 @@ import Head from "next/head";
 import HeaderGeneric from "../src/components/common/heaedrGeneric";
 import { Container, Input, Button, Form, FormGroup, Label } from "reactstrap";
 import Footer from "../src/components/common/footer";
+import { FormEvent, useState } from "react";
+import authService from "../src/services/authService";
+import { useRouter } from "next/router";
+import ToastComponent from "../src/components/common/toast";
 
 const Register = function () {
+    const router = useRouter();
+    const [toastIsOpen, setToastIsOpen] = useState(false);
+    const [toastMessage, setToastMessage] = useState("");
+    const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+   
+
+    const formData = new FormData(event.currentTarget);
+    const firstName = formData.get("firstName")!.toString();
+    const lastName = formData.get("lastName")!.toString();
+    const phone = formData.get("phone")!.toString();
+    const birth = formData.get("birth")!.toString();
+    const email = formData.get("email")!.toString();
+    const password = formData.get("password")!.toString();
+    const confirmPassword = formData.get("confirmPassword")!.toString();
+    const params = { firstName, lastName, phone, birth, email, password };
+
+    if (password != confirmPassword) {
+        setToastIsOpen(true);
+        setTimeout(() => {
+        setToastIsOpen(false);
+    }, 1000 * 3);
+        setToastMessage("Senha e confirmação diferentes.");
+          
+      return;
+    }
+
+    const {data, status} = await authService.register(params)
+     
+     
+    if (status === 201) {
+        router.push("/login?sucess=true");
+    } else {
+        setToastIsOpen(true);
+      setTimeout(() => {
+      setToastIsOpen(false);
+    }, 1000 * 3);
+        setToastMessage(data.message);
+    }
+  };
+
     return (
         <>
             <Head>
@@ -16,7 +61,7 @@ const Register = function () {
                 <Container className="py-5">
                     <HeaderGeneric logoUrl="/" btnUrl="/login" btnContent="Quero fazer login" />
                     <p className={styles.formTitle}>Bem-vindo(a) ao OneBitFlix!</p>
-                    <Form className={styles.form} >
+                    <Form className={styles.form} onSubmit={handleRegister}>
                         <p className="text-center">
                             <strong>Bem-vindo(a) ao OneBitFlix!</strong>
                         </p>
@@ -125,6 +170,7 @@ const Register = function () {
                     </Form>
                 </Container>
                 <Footer/>
+                <ToastComponent color="bg-danger" isOpen={toastIsOpen} message={toastMessage}/>
             </main>
         </>
 
