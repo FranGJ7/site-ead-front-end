541dab995682a634e32cd1fe75c2fd2fc249f1f1
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-09-21 15:41:54 -0300

    Seção de destaque finalizada

--- src/components/homeAuth/featuresSection/index.tsx
+++ src/components/homeAuth/featuresSection/index.tsx
@@ -22,20 +22,24 @@ const FeaturedSection = function () {
     <>
     {data.data.map((course: CourseType)=>(
     
-        <div key={course.id}>
+        <div style={{
+            backgroundImage: `linear-gradient(to bottom, #6666661a, #151515), url(${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "480px",
+            }} 
+            key={course.id}>
             <HeaderAuth/>
-            <Container>
+            <Container className="pt-4">
                <p className={styles.title}>{course.name}</p>
                <p className={styles.description}>{course.synopsis}</p>
                <Link href={`/courses/${course.id}`}>
-                 <Button outline color="light">
+                 <Button outline color="light" className={styles.button}>
                     ACESSE AGORA!
                     <img src="/buttonPlay.svg" alt="buttonImg" className={styles.buttonImg}/>
                  </Button>
                </Link>
-
             </Container>
-        <p>{course.name}</p>
         </div>
 
 
